225e1d87e7a4b3948ace9484390c7e99c0dab8b1
Shudhanshu <saggreener@gmail.com>
2023-11-01 23:19:42 +0530

    Adding files of project

---
+++ app/src/main/java/sag/project/demoapp/MainActivity.kt
@@ -0,0 +1,15 @@
+package sag.project.demoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
+
